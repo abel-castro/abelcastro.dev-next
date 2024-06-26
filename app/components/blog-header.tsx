328856655b08f19d81c2b7da8acc69545826a35b
Abel Castro <hi@abelcastro.dev>
2024-06-26 06:16:33 +0200

    Add header rainbow effect

--- app/components/blog-header.tsx
+++ app/components/blog-header.tsx
@@ -2,7 +2,7 @@ export default async function BlogHeader() {
   const rootURL = process.env.ROOT_URL;
   return (
     <header className="text-white p-4 mb-8">
-      <h1 className="text-3xl font-bold">
+      <h1 className="text-3xl font-bold rainbow-text">
         <a href={rootURL}>AbelCastro.Dev</a>
       </h1>
     </header>
