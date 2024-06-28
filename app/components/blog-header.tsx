b33e6d2479098695d5fed9c37124be230c914cee
Abel Castro <hi@abelcastro.dev>
2024-06-28 06:45:03 +0200

    Fix blog-header error if env is missing

--- app/components/blog-header.tsx
+++ app/components/blog-header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Search from "./posts/post-search";
 
 export default async function BlogHeader() {
-  const rootURL = process.env.ROOT_URL;
+  const rootURL = process.env.ROOT_URL ?? "";
   return (
     <header className="flex flex-col justify-between sm:flex-row sm:justify-between p-4 mb-8">
       <h1 className="text-3xl font-bold rainbow-text text-center sm:text-left">
