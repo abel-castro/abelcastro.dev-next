e855121d2c4ff6033374a36e5848b813d54f5465
Abel Castro <hi@abelcastro.dev>
2024-07-16 11:32:09 +0200

    Remove unnecessary main tag

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -2,7 +2,6 @@ import "../globals.css";
 import BlogHeader from "../components/blog-header";
 import BlogFooter from "../components/blog-footer";
 
-
 export default function Layout({
   children,
 }: Readonly<{
@@ -11,11 +10,7 @@ export default function Layout({
   return (
     <div className="min-h-screen flex flex-col dark:bg-slate-900">
       <BlogHeader />
-
-      <main className="flex-grow p-4">
-        <div className="max-w-4xl mx-auto">{children}</div>
-      </main>
-
+      <div className="max-w-4xl mx-auto">{children}</div>
       <BlogFooter />
     </div>
   );
