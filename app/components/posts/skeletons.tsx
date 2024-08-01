92095e170fb3fa7146e3aefa9ab208397ac8b04d
Abel Castro <hi@abelcastro.dev>
2024-08-01 10:13:33 +0200

    Fix single post skeleton

--- app/components/posts/skeletons.tsx
+++ app/components/posts/skeletons.tsx
@@ -1,16 +1,18 @@
 export function PostContentSkeleton() {
   return (
-    <div className="animate-pulse flex flex-col space-y-4 shadow-md">
-      <div className="h-4 bg-sky-100 rounded w-full" />
-      <div className="flex rounded bg-sky-100 px-8 py-12 mt-2" />
-      <div className="h-4 bg-sky-100 rounded w-full" />
-      <div className="h-4 bg-sky-100 rounded w-5/6" />
-      <div className="h-4 bg-sky-100 rounded w-4/6" />
-      <div className="flex rounded bg-sky-100 px-8 py-12 mt-2" />
-      <div className="h-4 bg-sky-100 rounded w-full" />
-      <div className="h-4 bg-sky-100 rounded w-full" />
-      <div className="h-4 bg-sky-100 rounded w-5/6" />
-      <div className="h-4 bg-sky-100 rounded w-4/6" />
+    <div className="container mx-auto mt-8">
+      <div className="animate-pulse flex flex-col space-y-4 shadow-md">
+        <div className="h-4 bg-sky-100 rounded w-full" />
+        <div className="flex rounded bg-sky-100 px-8 py-12 mt-2" />
+        <div className="h-4 bg-sky-100 rounded w-full" />
+        <div className="h-4 bg-sky-100 rounded w-5/6" />
+        <div className="h-4 bg-sky-100 rounded w-4/6" />
+        <div className="flex rounded bg-sky-100 px-8 py-12 mt-2" />
+        <div className="h-4 bg-sky-100 rounded w-full" />
+        <div className="h-4 bg-sky-100 rounded w-full" />
+        <div className="h-4 bg-sky-100 rounded w-5/6" />
+        <div className="h-4 bg-sky-100 rounded w-4/6" />
+      </div>
     </div>
   );
 }
