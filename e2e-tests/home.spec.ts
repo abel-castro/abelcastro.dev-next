66b86ec60d4c00f1ad7d28549592afc567fb47c3
Abel Castro <hi@abelcastro.dev>
2024-08-06 06:39:36 +0200

    Fix playwright baseURL

--- e2e-tests/home.spec.ts
+++ e2e-tests/home.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Locator } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test("main elements are visible", async ({ page }) => {
   await page.goto("/");
