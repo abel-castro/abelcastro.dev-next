97e969223cbcf585fff3aa8d0aafc175b48c3116
Abel Castro <hi@abelcastro.dev>
2024-08-12 10:21:55 +0200

    Fix issue with rehypePlugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
-// next.config.mjs
-import withMDX from "@next/mdx";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const mdxOptions = {
-  extension: /\.mdx?$/,
-  options: {
-    rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, rehypeHighlight],
-  },
-};
-
-const nextConfig = {
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-};
-
-export default withMDX(mdxOptions)(nextConfig);
+export default nextConfig;
