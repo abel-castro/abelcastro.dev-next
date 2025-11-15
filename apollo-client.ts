661a0765fcc2db8327eee9aab8fbd28a6b41ee64
Abel Castro <hi@abelcastro.dev>
2025-11-15 01:14:51 +0100

    Try to fix vercel deployment issues

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,9 +1,17 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { HttpLink } from '@apollo/client/link/http';
 
+const graphqlUrl = process.env.NEXT_PUBLIC_BLOG_GRAPHQL_URL;
+
+if (!graphqlUrl) {
+    throw new Error(
+        'NEXT_PUBLIC_BLOG_GRAPHQL_URL environment variable is not set. Please configure it in your environment variables.',
+    );
+}
+
 const apolloClient = new ApolloClient({
     link: new HttpLink({
-        uri: process.env.NEXT_PUBLIC_BLOG_GRAPHQL_URL,
+        uri: graphqlUrl,
         fetchOptions: {
             mode: 'cors',
         },
